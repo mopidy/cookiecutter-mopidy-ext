0ad5f88bc79b444134b11a7ef9fca16ed1f0be01
Stein Magnus Jodal <stein.magnus@jodal.no>
2013-10-06 23:38:53 +0200

    setup: Find Python packages automatically

--- {{cookiecutter.repo_name}}/setup.py
+++ {{cookiecutter.repo_name}}/setup.py
@@ -1,7 +1,7 @@
 from __future__ import unicode_literals
 
 import re
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
 def get_version(filename):
@@ -19,7 +19,7 @@ setup(
     author_email='{{ cookiecutter.author_email }}',
     description='{{ cookiecutter.short_description }}',
     long_description=open('README.rst').read(),
-    packages=['mopidy_{{ cookiecutter.ext_name }}'],
+    packages=find_packages(exclude=['tests', 'tests.*']),
     zip_safe=False,
     include_package_data=True,
     install_requires=[
