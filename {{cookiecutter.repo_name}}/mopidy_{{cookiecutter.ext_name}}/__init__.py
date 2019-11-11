6c7ebb5e380a2d848c099c8b349b7e51d4e5114a
Stein Magnus Jodal <stein.magnus@jodal.no>
2019-11-11 01:14:10 +0100

    Simplify super() call

--- {{cookiecutter.repo_name}}/mopidy_{{cookiecutter.ext_name}}/__init__.py
+++ {{cookiecutter.repo_name}}/mopidy_{{cookiecutter.ext_name}}/__init__.py
@@ -21,7 +21,7 @@ class Extension(ext.Extension):
         return config.read(pathlib.Path(__file__).parent / "ext.conf")
 
     def get_config_schema(self):
-        schema = super(Extension, self).get_config_schema()
+        schema = super().get_config_schema()
         # TODO: Comment in and edit, or remove entirely
         #schema["username"] = config.String()
         #schema["password"] = config.Secret()
